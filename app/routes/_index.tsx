ac5b3ffe6efef80d51307d1200920facec52ca47
Andrew Huth <git@huth.me>
2025-01-13 22:13:15 -0500

    Remix forms

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,17 +1,19 @@
+import {Form} from "@remix-run/react";
+
 export default function Index() {
   return (
     <div className="flex h-screen flex-col items-center gap-8">
       <h1 className="text-balance pt-[min(28rem,25vh)] text-5xl">
         The fastest LLM chat app
       </h1>
-      <form className="w-full sm:max-w-3xl" method="post">
+      <Form className="w-full sm:max-w-3xl" method="post">
         <textarea
           aria-label="Message"
           className="h-24 w-full resize-none rounded-lg p-3"
           name="chat"
           placeholder="Type your message here..."
         />
-      </form>
+      </Form>
       <footer className="mt-auto p-4">
         <p>
           By sending a message, you agree to our Terms of service and have read
--- app/routes/chat._index.tsx
+++ app/routes/chat._index.tsx
@@ -1,14 +1,16 @@
+import {Form} from "@remix-run/react";
+
 export default function ChatIndex() {
   return (
     <div className="flex h-full flex-col items-center justify-end px-4 pt-4">
-      <form className="w-full sm:max-w-3xl" method="post">
+      <Form className="w-full sm:max-w-3xl" method="post">
         <textarea
           aria-label="Message"
           className="h-24 w-full resize-none rounded-lg rounded-b-none p-3"
           name="chat"
           placeholder="Type your message here..."
         />
-      </form>
+      </Form>
     </div>
   );
 }
